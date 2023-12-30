e6529c9f43146a1d0dc4c3e82fc5f134e18d3be8
N1Code7 <n1code7@outlook.fr>
2023-12-30 23:03:15 +0100

    🤡 add fake user

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -1,8 +1,16 @@
 import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
 import { UserFactory } from "../factories";
+import User from "../../app/Models/User";
 
 export default class extends BaseSeeder {
   public async run() {
     await UserFactory.createMany(10);
+    await User.create({
+      lastName: "mafféïs",
+      firstName: "nicolas",
+      email: "nico@mail.com",
+      password: "password",
+      roles: ["ROLE_SUPERADMIN"],
+    });
   }
 }
