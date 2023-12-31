a5295fb8701a09f5efa614e0f79436786ab8633c
N1Code7 <n1code7@outlook.fr>
2023-12-31 02:47:06 +0100

    🧑‍💻 use middleware for authentication

--- start/routes.ts
+++ start/routes.ts
@@ -5,26 +5,40 @@
 */
 
 import Route from "@ioc:Adonis/Core/Route";
-import User from "../app/Models/User";
+import User from "#models/User";
 
-Route.get("/", async ({ session }) => {
+Route.get("/", async ({}) => {
   const users = await User.all();
-  console.log(session.all());
   return users;
-});
-//.middleware("auth");
+}).middleware("auth");
 
+/**
+ * Group prefixed by "/api"
+ */
 Route.group(() => {
   /**
-   * Auth
+   * PRIVATE
    */
-  Route.post("/login", "AuthController.login");
-  Route.post("/logout", "AuthController.logout");
-  Route.post("/register", "AuthController.register");
+  Route.group(() => {
+    /**
+     * User
+     */
+    Route.get("/me", "UsersController.me");
+    Route.get("/user/:id", "UsersController.show");
+    Route.get("/users", "UsersController.show");
+  }).middleware("auth");
 
   /**
-   * User
+   * PUBLIC
    */
-  Route.get("/me", "UsersController.me");
+  Route.group(() => {
+    /**
+     * Auth
+     */
+    Route.post("/login", "AuthController.login");
+    Route.post("/logout", "AuthController.logout");
+    Route.post("/register", "AuthController.register");
+  });
+
   //
 }).prefix("/api");
