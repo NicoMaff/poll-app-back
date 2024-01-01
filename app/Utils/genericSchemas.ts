8d4840b57f14c0b8faccd306c16c96620ed5e27f
N1Code7 <n1code7@outlook.fr>
2024-01-01 01:27:19 +0100

    🧑‍💻 add generic schema

--- app/Utils/genericSchemas.ts
+++ app/Utils/genericSchemas.ts
@@ -3,6 +3,11 @@ import { schema, rules } from "@ioc:Adonis/Core/Validator";
 export const UuidSchema = schema.create({
   id: schema.string([rules.uuid()]),
 });
+
 export const OptionalUuidSchema = schema.create({
   id: schema.string.optional([rules.uuid()]),
+});
+
+export const emailSchema = schema.create({
+  email: schema.string([rules.email()]),
 });
